9ead1a31718fb3ab9dca5cca7c28c10f12235e4b
Anurag Pandey <64393814+anuragpandey1115@users.noreply.github.com>
2020-12-22 20:20:49 +0530

    Add files via upload

---
+++ src/robots.js
@@ -0,0 +1,39 @@
+export const robots = [
+{
+	id: 1,
+	name: 'Anurag Pandey',
+	username: 'baapji',
+	email: 'baapji@gmail.com'
+},
+{
+	id: 2,
+	name: 'ritu singh',
+	usernamr: 'rs',
+	email: 'rs@mail.com'
+
+},
+{
+	id:3,
+	name: 'sunny pandey',
+	usernamr: 'sunny',
+	email: 'sunny@gmail.com'
+},
+{
+	id:4,
+	name: 'alan sinha',
+	usernamr: 'lala',
+	email: 'lala@gmail.com'
+},
+{
+	id:5,
+	name: 'lala sinha',
+	usernamr: 'budha',
+	email: 'bugha@gmail.com'
+},
+{
+	id:6,
+	name: 'ashish',
+	usernamr: 'ashishwa',
+	email: 'ashish@gmail.com'
+}
+]
